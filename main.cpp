9705c6b0b379a6bb8d3aae83a8c974f3eb39dfbe
Roushan kumar❣️ <roushankumar@Roushan-MacBook-Air.local>
2024-07-20 12:35:11 +0530

    code

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+const int N=1e6+5;
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cout<<"HEllo darlings";
+    return 0;
+}
